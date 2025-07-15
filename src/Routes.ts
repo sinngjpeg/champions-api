ad47426b267ef24681c543533c354415e0a6bd8b
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-15 19:43:52 -0300

    add route

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,9 +1,15 @@
-import { Router} from "express";
+import { Router } from "express";
 import * as PlayerController from "./controllers/PlayersController";
+import * as ClubsController from "./controllers/ClubsControler";
 
+const router = Router();
 
-const router = Router()
-router.get("/players", PlayerController.getPlayer)
-router.get("/players/:id", PlayerController.getPlayerById)
+router.get("/players", PlayerController.getPlayer);
+router.post("/players", PlayerController.postPlayer);
+router.delete("/players/:id", PlayerController.deletePlayer);
+router.patch("/players/:id", PlayerController.updatePlayer);
+router.get("/players/:id", PlayerController.getPlayerById);
+
+router.get("/clubs", ClubsController.getClubs);
 
 export default router;
