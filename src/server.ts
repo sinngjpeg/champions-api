88a3a4227c8e60a05bafb493850c73173e30192e
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-15 15:40:16 -0300

    initial server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+import express, {Request, Response} from "express";
+
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (request: Request, response: Response) => {
+    response.send("Hello World!");
+});
+
+
+app.listen(port, ()=> {
+    console.log(`Server running at port http://localhost:${port}`)
+});
+
+
