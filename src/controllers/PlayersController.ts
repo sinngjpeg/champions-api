d5b9ef1f776b60d86841d89087066bc3020d996c
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-15 16:19:26 -0300

    add utils layer

--- src/controllers/PlayersController.ts
+++ src/controllers/PlayersController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { getPlayerService } from "../services/PlayersService";
+import { ok } from "../utils/HttpHelper";
 
 
 export const getPlayer = async(request: Request, response: Response) =>{
     const data = await getPlayerService();
-    response.status(200).json(data);
+    const res = await ok(data)
+    response.status(res.statusCode).json(res.body);
 };
